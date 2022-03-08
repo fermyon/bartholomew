776f379bd02ba0b766bb577add4497d7b10c2fdf
Matt Butcher <matt.butcher@fermyon.com>
2022-03-07 18:00:18 -0700

    Remove unused field from siteinfo.
    Signed-off-by: Matt Butcher <matt.butcher@fermyon.com>

--- src/template.rs
+++ src/template.rs
@@ -17,7 +17,6 @@ pub struct SiteInfo {
     pub title: String,
     pub logo: Option<String>,
     pub base_url: Option<String>,
-    pub content_encoding: Option<String>,
     pub about: Option<String>,
     pub extra: BTreeMap<String, String>,
 }
